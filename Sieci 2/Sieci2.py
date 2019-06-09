9e1b51ef5191ae44e726dae611c4edb67bcfae88
kryrad <48049544+kryrad@users.noreply.github.com>
2019-06-09 12:06:21 +0200

    Add files via upload

---
+++ Sieci 2/Sieci2.py
@@ -0,0 +1,117 @@
+import keras
+import sys
+from matplotlib import pyplot
+from keras.models import Sequential
+from keras.utils import np_utils
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
+from keras.datasets import cifar10
+from keras import regularizers
+from keras.callbacks import LearningRateScheduler
+import numpy as np
+import timeit
+
+def lr_schedule(epoch):
+    lrate = 0.001
+    if epoch > 75:
+        lrate = 0.0005
+    elif epoch > 100:
+        lrate = 0.0003        
+    return lrate
+
+# plot diagnostic learning curves
+def summarize_diagnostics(history, name):
+	# plot loss
+	pyplot.subplot(211)
+	pyplot.title('Cross Entropy Loss')
+	pyplot.plot(history.history['loss'], color='blue', label='train')
+	pyplot.plot(history.history['val_loss'], color='orange', label='test')
+	# plot accuracy
+	pyplot.subplot(212)
+	pyplot.title('Classification Accuracy')
+	pyplot.plot(history.history['acc'], color='blue', label='train')
+	pyplot.plot(history.history['val_acc'], color='orange', label='test')
+	# save plot to file
+	pyplot.savefig(name + '_plot.png')
+	pyplot.close()
+	
+def define_model():
+    weight_decay = 1e-4
+    model = Sequential()
+    model.add(Conv2D(32, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay), input_shape=x_train.shape[1:]))
+    model.add(Activation('elu'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(32, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('elu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(64, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('elu'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('elu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.3))
+
+    model.add(Conv2D(128, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('elu'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(128, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(Activation('elu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.4))
+
+    model.add(Flatten())
+    model.add(Dense(num_classes, activation='softmax'))
+    return model
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+#z-score
+mean = np.mean(x_train,axis=(0,1,2,3))
+std = np.std(x_train,axis=(0,1,2,3))
+x_train = (x_train-mean)/(std+1e-7)
+x_test = (x_test-mean)/(std+1e-7)
+
+num_classes = 10
+y_train = np_utils.to_categorical(y_train,num_classes)
+y_test = np_utils.to_categorical(y_test,num_classes)
+
+#data augmentation
+datagen = ImageDataGenerator(rotation_range=15,width_shift_range=0.1,height_shift_range=0.1,horizontal_flip=True,)
+datagen.fit(x_train)
+
+#training
+start = timeit.default_timer() 
+
+batch_size = 64
+model = define_model()
+opt_rms = keras.optimizers.rmsprop(lr=0.001,decay=1e-6)
+model.compile(loss='categorical_crossentropy', optimizer=opt_rms, metrics=['accuracy'])
+history = model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),\
+                    steps_per_epoch=x_train.shape[0] // batch_size,epochs=125,\
+                    verbose=1,validation_data=(x_test,y_test),callbacks=[LearningRateScheduler(lr_schedule)])
+
+#testing
+scores = model.evaluate(x_test, y_test, batch_size=128, verbose=1)
+print('\nTest result: %.3f loss: %.3f' % (scores[1]*100,scores[0]))
+    
+summarize_diagnostics(history,'1')
+
+from sklearn.metrics import confusion_matrix
+
+predict_classes = model.predict_classes(x_test[1:10000,], batch_size=32)
+true_classes = np.argmax(y_test[1:10000],1)
+
+print(confusion_matrix(true_classes, predict_classes))
+stop = timeit.default_timer()
+print('Time: ', stop - start)
+    
+input("Press enter to exit")
---
+++ Sieci 2/testy/Readme.txt
@@ -0,0 +1,8 @@
+1 bez zmian
+2 epoch 80
+3 epoch 170
+4 2 VGG Blocks zamiast 3
+5 learning rate 0.001 zamiast zmiennego
+
+Wiele rzeczy jest fajnie opisane w linku który Ci wczeœniej wys³a³em:
+https://machinelearningmastery.com/how-to-develop-a-cnn-from-scratch-for-cifar-10-photo-classification/
